ea0641a46843cfd8b5307dc4badbad4e113e26b9
Amitha Shenoy <amita13_shenoy@yahoo.com>
2019-10-11 00:37:06 +0530

    remove config

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -12,7 +12,7 @@ namespace MVC_Tutorial
             // Web API configuration and services
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
+           //  config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
